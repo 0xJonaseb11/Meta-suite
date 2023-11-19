930429179a1621cb87f01398572f0bda43e83807
jonas-sebera <sebejaz99@gmail.com>
2023-11-19 22:07:17 +0200

    update

--- meta-suite/src/App.js
+++ meta-suite/src/App.js
@@ -1,56 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
 import { useEffect, useState } from "react";
 import { Contract, providers } from "ethers";
 
-import React from 'react'
-
-const App = () => {
+function App() {
 
   const [isWalletInstalled, setIsWalletInstalled] = useState(false);
-  
-  // keep track of connected accounts(current)
+  // state for keeping track of current connected account.
   const [account, setAccount] = useState(null);
 
   useEffect(() => {
+        if (window.ethereum) {
+            setIsWalletInstalled(true);
+        }
+    }, []);
 
-    if (window.ethereum) {
-      setIsWalletInstalled(true);
-
+  async function connectWallet() {
+        window.ethereum
+            .request({
+                method: "eth_requestAccounts",
+            })
+            .then((accounts) => {
+                setAccount(accounts[0]);
+            })
+            .catch((error) => {
+                alert("Something went wrong");
+            });
     }
 
-  }, []);
-  
-  const connectWallet = async () => {
-    window.ethereum
-        .request({
-          method: "eth_requestAccounts",
-        })
-        .then((accounts) {
-          setAccount(account[0]);
-        })
-        .catch((error) => {
-          alert("Something went wrong");
-        });
+    if (account === null) {
+      return (
+        <div className="App">
+          {
+            isWalletInstalled ? (
+              <button onClick={connectWallet}>Connect Wallet</button>
+            ) : (
+              <p>Install Metamask wallet</p>
+            )
+          }
 
-  }
-
-  if (account == null) {
-    return (
-      <div className="App">
-        {
-          isWalletInstalled ? (
-            <button onClick={connectWallet}>Connet Wallet</button>
-          ) : (
-            <p>Install wallet</p>
-          )
-        }
-      </div>
+        </div>
+      );
+    }
+      return (
+        <div className="App">
+          <p>Connected as: {account}</p>
+        </div>
     );
   }
-  return (
-    <div className="App">
-      <p>Connected as : {account}</p>
-    </div>
-  );
-}
-
-export default App
+export default App;

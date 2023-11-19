59f56e31320abb6bf54a2c71efb0bdfd883295d5
jonas-sebera <sebejaz99@gmail.com>
2023-11-19 20:01:47 +0200

    init update

---
+++ meta-suite/src/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { Contract, providers } from "ethers";
+
+import React from 'react'
+
+const App = () => {
+
+  const [isWalletInstalled, setIsWalletInstalled] = useState(false);
+  return (
+    <div>App</div>
+  )
+}
+
+export default App

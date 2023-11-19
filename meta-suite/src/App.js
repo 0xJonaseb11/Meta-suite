d60764fc08e367dd0f8bc1d47993b98c248ee414
jonas-sebera <sebejaz99@gmail.com>
2023-11-19 20:13:25 +0200

    connectWallet init

--- meta-suite/src/App.js
+++ meta-suite/src/App.js
@@ -6,6 +6,31 @@ import React from 'react'
 const App = () => {
 
   const [isWalletInstalled, setIsWalletInstalled] = useState(false);
+  
+  // keep track of connected accounts(current)
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+
+    if (window.ethereum) {
+      setIsWalletInstalled(true);
+
+    }
+
+  }, []);
+  
+  const connectWallet = async () => {
+    window.ethereum
+        .request({
+          method: "eth_requestAccounts",
+        })
+        .then((accounts) {
+          setAccount(account[0]);
+        })
+        .catch((error) => {
+          alert("Something went wrong");
+        });
+  }
   return (
     <div>App</div>
   )

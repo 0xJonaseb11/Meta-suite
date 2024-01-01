41863a0f334bc8fd8bfe5fff2eeab4adf5a91923
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-01 18:02:40 +0200

    init tests

--- meta-suite/src/App.test.js
+++ meta-suite/src/App.test.js
@@ -6,3 +6,9 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+
+import react from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+

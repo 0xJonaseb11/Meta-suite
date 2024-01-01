b4db31fd8a2f8d3f39894fcc811e26997bbc8a45
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-01 18:04:15 +0200

    init tests update

--- meta-suite/src/App.test.js
+++ meta-suite/src/App.test.js
@@ -12,3 +12,9 @@ import react from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+it ('renders wthout crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <App />, div
+  )
+});
